8bcd7d0d1f02aa839ef7cbe33a5aa0ccde75525c
Neema <nrezaie12@gmail,com>
2022-04-16 01:36:04 -0400

    commit tree f14d0add5398bbe93caede0e96be8c9165338ee7

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+function addElement () {
+    const newHeader = document.createElement('h1#victory')
+    document.getElementById(newHeader).textContent = "YOUR-NAME is the champion"
+    newDiv.appendChild(newHeader)
+}
